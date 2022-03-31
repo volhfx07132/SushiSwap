f3748347a38edc6cd2dc41fc11bebdc1502abe16
Le Hong vo <volhfx07132@funix.edu.vn>
2022-03-31 11:29:47 +0700

    Update SushiSwapFactoryMock

--- contracts/mocks/SushiSwapFactoryMock.sol
+++ contracts/mocks/SushiSwapFactoryMock.sol
@@ -5,5 +5,6 @@ pragma solidity 0.6.12;
 import "../uniswapv2/UniswapV2Factory.sol";
 
 contract SushiSwapFactoryMock is UniswapV2Factory {
+    // Set fee to transaction, only address in MasterChefV2 list can chage
     constructor(address _feeToSetter) public UniswapV2Factory(_feeToSetter) {}
 }
